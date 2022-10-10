2f860caeb3e3b7c17e2e297f2411fb84d90f1d0b
Md Rahi <fhrfhr102030@gmail.com>
2022-10-10 21:31:38 +0600

    Header Section Design Done

---
+++ src/components/Header/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div className='bg-slate-200'>
+            <nav className='grid grid-cols-1 gap-3 lg:flex md:grid-cols-2 justify-between mx-6 p-4'>
+                <div className='font-semibold'>
+                    <NavLink className='' to={'/home'}><span className='text-3xl text-emerald-600'>Back</span> Toppers</NavLink>
+                </div>
+                <div className='grid grid-cols-1 lg:grid-cols-3 md:grid-cols-3 items-center   gap-1 lg:gap-4 font-bold'>
+                    <NavLink className={({isActive}) => isActive ? 'text-emerald-600 rounded': undefined} to={'/home'}>Home</NavLink>
+                    <NavLink className={({isActive}) => isActive ? 'text-emerald-600 rounded': undefined} to={'/statistics'}>Statistics</NavLink>
+                    <NavLink className={({isActive}) => isActive ? 'text-emerald-600 rounded': undefined} to={'/blog'}>Blog</NavLink>
+                </div>
+                <div className='hidden lg:flex'>
+                    <NavLink className='' to={'/blog'}>About</NavLink>
+                </div>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
