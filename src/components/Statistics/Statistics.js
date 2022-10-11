6575b11a61ad5f4bc3f4545b228b3aed72c8ad35
Md Rahi <fhrfhr102030@gmail.com>
2022-10-11 19:53:45 +0600

    fix charts responsive  issue

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 import { Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
-import './Statistics.css';
 
 const Statistics = () => {
    const chart = useLoaderData();
    console.log(chart.data)
    const data = chart.data;
     return (
-        <div className='w-3/6 mx-auto my-20 p-8 bg-gray-200'>
-            <LineChart width={600} height={400} data={data}>
+        <div className='m-2 p-8 bg-gray-200'>
+            <LineChart className='w-full mx-auto' width={400} height={400} data={data}>
                 <Line type='monotone' dataKey='total' stroke='#82ca9d'></Line>
                     <XAxis dataKey='name'></XAxis>
                     <YAxis dataKey=''></YAxis>
