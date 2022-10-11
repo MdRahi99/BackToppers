c773a22703e96d279b9680dddecde35ee0447d50
Md Rahi <fhrfhr102030@gmail.com>
2022-10-11 15:32:22 +0600

    Show correct ans design done

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,18 +1,45 @@
-import React from 'react';
-import Options from '../Options/Options';
+import React, { useState } from 'react';
+import { EyeIcon } from '@heroicons/react/24/solid'
 
-const Details = ({question}) => {
+const Details = ({ques}) => {
+
+    const {options, question, correctAnswer} =ques;
+    console.log(ques)
+    const [ansBtn, setAnsBtn] = useState([]);
+    const handleBtn = () => {
+        setAnsBtn(<h1 className='bg-gray-500 text-white py-3 w-5/6 lg:w-3/6 mx-auto mt-12 rounded'>correct Answer: <span className='font-bold'>{correctAnswer}</span></h1>)
+    }
+
     return (
         <div className='shadow-lg bg-gray-200 p-4 my-8'>
-            <h1 className='text-1xl lg:text-2xl font-semibold mb-6'>{question.question}</h1>
-            <div className='grid grid-cols-1 lg:grid-cols-2 gap-3 w-4/6 mx-auto'>
-            {
-                question.options.map(option => <Options
-                    key={option.id}
-                    option={option}
-                ></Options>)
-            }
+            <div className='flex justify-between mx-3 lg:mx-20'>
+                <h1 className='text-1xl lg:text-2xl font-semibold mb-6'>{question}</h1>
+                <EyeIcon onClick={() => handleBtn()} className="h-6 w-6 text-gray-800"/>
             </div>
+            
+            <div className='grid grid-cols-1 lg:grid-cols-2 gap-2 w-5/6 mx-auto'>
+                <div className='flex w-5/6 mx-auto'>
+                    <input type="radio" name="radio" className="radio radio-secondary"/>
+                    <p className='ml-4'>{options[0]}</p> 
+                </div>
+
+                <div className='flex w-5/6 mx-auto'>
+                    <input type="radio" name="radio" className="radio radio-secondary"/>
+                    <p className='ml-4'>{options[1]}</p> 
+                </div>
+
+                <div className='flex w-5/6 mx-auto'>
+                    <input type="radio" name="radio" className="radio radio-secondary"/>
+                    <p className='ml-4'>{options[2]}</p> 
+                </div> 
+
+                <div className='flex w-5/6 mx-auto'>
+                    <input type="radio" name="radio" className="radio radio-secondary"/>
+                    <p className='ml-4'>{options[3]}</p> 
+                </div> 
+            </div>
+
+            {ansBtn}
         </div>
     );
 };
--- src/components/TopicDetails/TopicDetails.js
+++ src/components/TopicDetails/TopicDetails.js
@@ -4,18 +4,20 @@ import Details from '../Details/Details';
 
 const TopicDetails = () => {
     const topic = useLoaderData();
+    const quiz = topic.data;
+    const {questions} = quiz;
 
     return (
         <div>
-            <h1 className='text-3xl p-4 mt-6'>Selected Topic: <span className='font-semibold text-4xl'>{topic.data.name}</span></h1>
-            <p className='text-2xl'>Total Questions: {topic.data.total}</p>
+            <h1 className='text-3xl p-4 mt-6'>Selected Topic: <span className='font-semibold text-4xl'>{quiz.name}</span></h1>
+            <p className='text-2xl'>Total Questions: {quiz.total}</p>
 
             <div className='shadow-md bg-gray-100 p-4 w-5/6 mx-auto my-12'>
                 <p className='font-semibold text-2xl'>Select correct answers:</p>
                 {
-                topic.data.questions.map(question => <Details
-                    key={question.id}
-                    question={question}
+                questions.map(ques => <Details
+                    key={ques.id}
+                    ques={ques}
                 ></Details>)
                 }
             </div>
